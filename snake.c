f0fcc6bd6bb7a5d93c98908f9c7b5ba268edeba0
Andrew Sims <andrew.sims@weployapp.com>
2020-08-07 08:17:57 +1000

    Handle keys

--- snake.c
+++ snake.c
@@ -2,20 +2,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define LEFT 'h'
-#define DOWN 'j'
-#define UP 'k'
-#define RIGHT 'l'
+#define DELAY 100000
 
 int max_rows;
 int max_cols;
 
+void handle_key(int key)
+{
+  erase();
+  switch (key)
+  {
+  case KEY_LEFT:
+    mvprintw(max_rows / 2, max_cols / 2, "%s", "left");
+    break;
+
+  case KEY_UP:
+    mvprintw(max_rows / 2, max_cols / 2, "%s", "up");
+    break;
+
+  case KEY_RIGHT:
+    mvprintw(max_rows / 2, max_cols / 2, "%s", "right");
+    break;
+
+  case KEY_DOWN:
+    mvprintw(max_rows / 2, max_cols / 2, "%s", "down");
+    break;
+
+  default:
+    mvprintw(max_rows / 2, max_cols / 2, "%s", "none");
+    break;
+  }
+}
+
 void game_loop(void)
 {
+  int counter = 0;
+
   while(1)
   {
-    char key = getch();
-    mvprintw(max_rows / 2, max_cols / 2, "%c", key);
+    usleep(DELAY);
+    int key = getch();
+    mvprintw(0, 0, "%d", counter);
+    handle_key(key);
+    counter++;
   }
 }
 
@@ -23,7 +52,10 @@ int main(void)
 {
   initscr();
   curs_set(0);
+  keypad(stdscr, 1);
   noecho();
+  cbreak();
+  nodelay(stdscr, 1);
   getmaxyx(stdscr, max_rows, max_cols);
 
   game_loop();
